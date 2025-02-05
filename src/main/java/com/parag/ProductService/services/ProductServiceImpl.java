dc6b8ad9a3b0bf4d43d78438cc8fd91aafa439e4
ParagShah97 <parag.shah97@gmail.com>
2025-02-05 16:44:45 -0500

    Made changes for config client and added reduce product function.

--- src/main/java/com/parag/ProductService/services/ProductServiceImpl.java
+++ src/main/java/com/parag/ProductService/services/ProductServiceImpl.java
@@ -5,7 +5,7 @@ import com.parag.ProductService.exceptions.ProductServiceCustomException;
 import com.parag.ProductService.model.ProductRequest;
 import com.parag.ProductService.model.ProductResponse;
 import com.parag.ProductService.repository.ProductRepository;
-import lombok.extern.log4j.Log4j2;
+//import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +15,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Service
-@Log4j2
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    private final Logger log = LogManager.getLogger(ProductServiceImpl.class);
     @Override
     public long addProduct(ProductRequest productRequest) {
         final Logger log = LogManager.getLogger(ProductServiceImpl.class);
@@ -40,12 +41,36 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponse getProductById(long id) {
-        final Logger log = LogManager.getLogger(ProductServiceImpl.class);
+
         log.info("Get the product for product id {}", id);
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ProductServiceCustomException("Product not found.", "PRODUCT_NOT_FOUND"));
         ProductResponse productResponse = new ProductResponse(product.getProductName(),
                 product.getProductId(), product.getPrice(), product.getQuantity());
         return productResponse;
+    }
+
+    @Override
+    public void reduceQuantity(long productId, long quantity) {
+        log.info("Reduce quantity {} for id: {}", quantity, productId);
+        // Using product Repo find the product by id, if not found then
+        // throw the exception.
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductServiceCustomException(
+                        "Product with given id not found",
+                        "PRODUCT_NOT_FOUND"
+                ));
+        // Here we check the quantity need to be reduced, else throw an exception.
+        if (product.getQuantity() < quantity) {
+            throw new ProductServiceCustomException(
+                    "Insufficient products quantity",
+                    "INSUFFICIENT_QUANTITY"
+            );
+        }
+        // Set the updated quantity to the product.
+        product.setQuantity(product.getQuantity() - quantity);
+        // Again Save the product
+        productRepository.save(product);
+        log.info("Product Quantity updated successfully.");
     }
 }
